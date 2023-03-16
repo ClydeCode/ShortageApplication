25caefe0157149923f4c26375ae8e18eb2d9b015
Clyde <noneclyde@gmail.com>
2023-03-16 10:06:25 +0200

    Add UserInput class with GetInt method

---
+++ ShortageApplication/UserInput.cs
@@ -0,0 +1,21 @@
+﻿
+namespace ShortageApplication
+{
+	internal class UserInput
+	{
+		public int GetInt(string title = "number")
+		{
+			Console.WriteLine($"\nType {title}:");
+
+			string? number = Console.ReadLine();
+
+			while (!int.TryParse(number, out _))
+			{
+				Console.WriteLine("Wrong number format!");
+				number = Console.ReadLine();
+			}
+
+			return int.Parse(number);
+		}
+	}
+}

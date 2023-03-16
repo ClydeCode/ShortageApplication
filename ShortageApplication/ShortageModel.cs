fcefe0a4f3f4346560485b95bfc8eb1e8ba4a632
Clyde <noneclyde@gmail.com>
2023-03-16 10:02:15 +0200

    Add ShortageModel class

---
+++ ShortageApplication/ShortageModel.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ShortageApplication
+{
+	internal class ShortageModel
+	{
+		public int Id { get; set; }
+		public string? Title { get; set; }
+		public string? Name { get; set; }
+		public string? Room { get; set; }
+		public string? Category { get; set; }
+		public int Priority { get; set; }
+		public DateTime CreatedOn { get; set; }
+	}
+}

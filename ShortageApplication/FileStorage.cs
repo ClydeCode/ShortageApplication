0f29bbb40335e66b106eb5d7ceecfeb76ce49bc0
Clyde <noneclyde@gmail.com>
2023-03-16 10:24:42 +0200

    Add FileStorage class with SaveData method

---
+++ ShortageApplication/FileStorage.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+
+namespace ShortageApplication
+{
+	internal class FileStorage
+	{
+		private string _fileName = "shortageData.json";
+
+		public void SaveData(List<ShortageModel> list)
+		{
+			string jsonString = JsonSerializer.Serialize(list);
+
+			File.WriteAllText(_fileName, jsonString);
+		}
+	}
+}

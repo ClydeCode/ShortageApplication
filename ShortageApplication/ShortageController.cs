ab63309556c1d67cfcf59d14b03c17614d6d2119
Clyde <noneclyde@gmail.com>
2023-03-16 10:04:22 +0200

    Add ShortageController class with several methods

---
+++ ShortageApplication/ShortageController.cs
@@ -0,0 +1,38 @@
+﻿
+namespace ShortageApplication
+{
+	internal class ShortageController
+	{
+		public void ShowMenu()
+		{
+			Console.Clear();
+			Console.WriteLine("\nMain Menu");
+			Console.WriteLine("\nWhat would you like to do:");
+			Console.WriteLine("\nType 0 To Close Application");
+			Console.WriteLine("Type 1 To View All Requests");
+			Console.WriteLine("Type 2 To Register a new Request");
+			Console.WriteLine("Type 3 To Delete a Request");
+		}
+
+		public void Navigate(int number)
+		{
+			Console.Clear();
+
+			switch (number)
+			{
+				case 0:
+					Environment.Exit(0);
+					break;
+				case 1:
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				default:
+					Console.WriteLine("Wrong option!");
+					break;
+			}
+		}
+	}
+}

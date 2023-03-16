dd211300da59d8215faa108ef6e221899d3c22f4
Clyde <noneclyde@gmail.com>
2023-03-16 12:45:47 +0200

    Rework methods in Filters class

--- ShortageApplication/Filters.cs
+++ ShortageApplication/Filters.cs
@@ -1,16 +1,18 @@
-﻿
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("TestApplication")]
 namespace ShortageApplication
 {
 	internal class Filters
 	{
 		public List<ShortageModel> FilterByTitle(List<ShortageModel> shortages, string word)
 		{
-			return shortages.Where(item => item.Title.Split(new char[] { ' ' }).Contains(word)).ToList();
+			return shortages.Where(item => item.Title.ToLower().Split(new char[] { ' ' }).Contains(word.ToLower())).ToList();
 		}
 
 		public List<ShortageModel> FilterByName(List<ShortageModel> shortages, string name)
 		{
-			return shortages.Where(item => item.Name == name).ToList();
+			return shortages.Where(item => item.Name.ToLower() == name.ToLower()).ToList();
 		}
 
 		public List<ShortageModel> FilterByDate(List<ShortageModel> shortages, DateTime startDate, DateTime endDate)
@@ -20,12 +22,12 @@ namespace ShortageApplication
 
 		public List<ShortageModel> FilterByCategory(List<ShortageModel> shortages, string category)
 		{
-			return shortages.Where(item => item.Category == category).ToList();
+			return shortages.Where(item => item.Category.ToLower() == category.ToLower()).ToList();
 		}
 
 		public List<ShortageModel> FilterByRoom(List<ShortageModel> shortages, string room)
 		{
-			return shortages.Where(item => item.Room == room).ToList();
+			return shortages.Where(item => item.Room.ToLower() == room.ToLower()).ToList();
 		}
 	}
 }

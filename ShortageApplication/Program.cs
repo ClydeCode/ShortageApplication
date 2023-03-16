040d1c7294706276dfd1305b4bd06bb020936242
Clyde <noneclyde@gmail.com>
2023-03-16 10:07:11 +0200

    Add navigation in application

--- ShortageApplication/Program.cs
+++ ShortageApplication/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using ShortageApplication;
+
+ShortageController Program = new();
+UserInput UserInput = new();
+
+while (true)
+{
+	Program.ShowMenu();
+
+	Program.Navigate(UserInput.GetInt());
+
+	Console.WriteLine("\nPress ENTER to Continue...");
+	Console.ReadLine();
+}
